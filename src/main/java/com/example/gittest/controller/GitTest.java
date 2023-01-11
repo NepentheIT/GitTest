102456362e3787342e8f748be854e7477da97a67
Lijian <651483313@qq.com>
2023-01-11 21:04:35 +0800

    second commit

--- src/main/java/com/example/gittest/controller/GitTest.java
+++ src/main/java/com/example/gittest/controller/GitTest.java
@@ -7,5 +7,6 @@ package com.example.gittest.controller;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
