edbf6cd64570ba5822cea6cb96dec256c4751522
Lijian <651483313@qq.com>
2023-01-11 21:11:02 +0800

    hot-fix first commit

--- src/main/java/com/example/gittest/controller/GitTest.java
+++ src/main/java/com/example/gittest/controller/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
