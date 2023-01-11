973d9dca1c162d607b2eae1ef617f2011f189fa6
Lijian <651483313@qq.com>
2023-01-11 21:14:01 +0800

    master commit

--- src/main/java/com/example/gittest/controller/GitTest.java
+++ src/main/java/com/example/gittest/controller/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
+        System.out.println("Hello master test");
     }
 }
